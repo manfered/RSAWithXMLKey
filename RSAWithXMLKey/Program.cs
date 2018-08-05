aab547aa79f531af3e427bc3f37b1cadda2b8427
manfered <35757736+manfered@users.noreply.github.com>
2018-08-05 17:31:38 -0600

    Add project files.

---
+++ RSAWithXMLKey/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSAWithXMLKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rsaWithXML = new RSAWithXMLKey();
+
+            const string message = "Message to Encrypt";
+            const string privateKeyPath = "c:\\temp\\privateKey.xml";
+            const string publicKeyPath = "c:\\temp\\publicKey.xml";
+
+            rsaWithXML.AssignNewKey(publicKeyPath, privateKeyPath);
+
+            var encrypted = rsaWithXML.Encrypt(publicKeyPath, Encoding.UTF8.GetBytes(message));
+            var decrypted = rsaWithXML.Decrypt(privateKeyPath, encrypted);
+
+            Console.WriteLine("RSA Encryption With XML Key");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine($"Message text : {message}");
+            Console.WriteLine($"Encrypted text : {Convert.ToBase64String(encrypted)}");
+            Console.WriteLine($"Decrypted text : {Encoding.UTF8.GetString(decrypted)}");
+
+
+        }
+    }
+}
---
+++ RSAWithXMLKey/RSAWithXMLKey.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace RSAWithXMLKey
+{
+    public class RSAWithXMLKey
+    {
+        private RSAParameters _publicKey;
+        private RSAParameters _privateKey;
+        
+        public void AssignNewKey(string publicKeyPath, string privateKeyPath)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                if (File.Exists(privateKeyPath))
+                {
+                    File.Delete(privateKeyPath);
+                }
+
+                if (File.Exists(publicKeyPath))
+                {
+                    File.Delete(publicKeyPath);
+                }
+
+                var publicKeyfolder = Path.GetDirectoryName(publicKeyPath);
+                var privateKeyfolder = Path.GetDirectoryName(privateKeyPath);
+
+                if (!Directory.Exists(publicKeyfolder))
+                {
+                    Directory.CreateDirectory(publicKeyfolder);
+                }
+
+                if (!Directory.Exists(privateKeyfolder))
+                {
+                    Directory.CreateDirectory(privateKeyfolder);
+                }
+
+                File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+                File.WriteAllText(privateKeyPath, rsa.ToXmlString(true));
+            }
+        }
+
+        public byte[] Encrypt(string publicKeyPath, byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+
+                cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+            }
+
+            return cipherbytes;
+        }
+
+        public byte[] Decrypt(string privateKeyPath, byte[] dataToDecrypt)
+        {
+            byte[] plain;
+
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+
+                plain = rsa.Decrypt(dataToDecrypt, false);
+            }
+
+            return plain;
+        }
+    }
+}
